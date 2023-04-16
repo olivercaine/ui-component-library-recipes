e8115733b82387beaf17234ed8990eae586239cf
Oliver Caine <olliecaine@gmail.com>
2023-04-16 14:10:41 +1000

    Exposing more (#14)
    * Exposing more components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { Button } from './ui/basics/Button'
+import { exampleRecipe } from './model/examples'
+import { Button } from './ui/basics/Button/Button'
+import { CookingInstructions } from './ui/components/CookingInstructions/CookingInstructions'
+import { Ingredients } from './ui/components/Ingredients/Ingredients'
 import { RecipePreview } from './ui/components/RecipePreview/RecipePreview'
+import { RecipeDetail } from './ui/features/RecipeDetail/RecipeDetail'
+import { Recipes } from './ui/features/Recipes/Recipes'
 
 export {
+  exampleRecipe,
   Button,
-  RecipePreview
+  CookingInstructions,
+  Ingredients,
+  RecipeDetail,
+  RecipePreview,
+  Recipes
 }
