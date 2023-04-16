d519d641f8858fe736db9fa0b8b655f03fdcee44
Oliver Caine <olliecaine@gmail.com>
2023-04-16 14:04:20 +1000

    Fix

--- src/ui/features/Recipes/Recipes.tsx
+++ src/ui/features/Recipes/Recipes.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import { Recipe } from '../../../model/types'
-import { RecipePreview } from '../../components/RecipePreview'
+import { RecipePreview } from '../../components/RecipePreview/RecipePreview'
 
 export interface IProps {
   /**
