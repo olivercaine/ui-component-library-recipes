1ff7df81e72381aa2920b1864e786a7ba68e2b1c
Oliver Caine <olliecaine@gmail.com>
2023-04-22 12:48:31 +1000

    Favourites icon + auto-adjust column heights (#23)

---
+++ src/ui/components/Favourite/Favourite.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta } from '@storybook/react'
+import { storyTemplate } from '../../../../.storybook/helpers'
+import { Favourite } from '../../../index'
+
+export default {
+  component: Favourite,
+  title: 'Components/Favourite',
+} as ComponentMeta<typeof Favourite>
+
+const template = storyTemplate(Favourite)
+
+const defaultArgs = {
+  // onChange: action('On Change'),
+}
+
+export const Default = template({ ...defaultArgs })
+
+export const Disabled = template({ ...defaultArgs })
--- src/ui/features/RecipeDetail/RecipeDetail.tsx
+++ src/ui/features/RecipeDetail/RecipeDetail.tsx
@@ -13,16 +13,16 @@ export interface IProps {
 
 export const RecipeDetail: FC<IProps> = ({ recipe }: IProps) =>
   <div className='grid grid-cols-3 gap-8 py-10 px-16 pb-20'>
-    <div className='col-span-2'>
+    <div className='col-span-3'>
       <RecipeHero recipe={recipe}/>
     </div>
-    <div className='rounded-lg'>
+    {/* <div className='rounded-lg'>
       <h2>Recipe info</h2>
       <ul>
         <li>Prep time 20 mins</li>
       </ul>
-    </div>
-    <div className='bg-light-grey rounded-lg shadow-xl'>
+    </div> */}
+    <div className='bg-light-grey self-start rounded-lg shadow-xl'>
       <Ingredients recipe={recipe}/>
     </div>
     <div className='col-span-2 bg-light-grey rounded-lg shadow-xl'>
