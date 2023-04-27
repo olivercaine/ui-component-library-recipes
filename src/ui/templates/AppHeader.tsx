49ebc99816da106cf8ab3eb1965da7e3018dafc9
Oliver Caine <olliecaine@gmail.com>
2023-04-27 15:48:33 +1000

    Pathname

--- src/ui/templates/AppHeader.tsx
+++ src/ui/templates/AppHeader.tsx
@@ -10,7 +10,7 @@ export const AppHeader = ({
 }) =>
   <header className='white-neutral-50 bg-dark-grey body-font' data-testid='AppHeader'>
     <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
-      <a className='flex title-font font-medium items-center mb-4 md:mb-0' href='/'>
+      <a className='flex title-font font-medium items-center mb-4 md:mb-0' href={window.location.pathname}>
         <svg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' stroke-linecap='round' viewBox='0 0 24 24'
           stroke-linejoin='round' stroke-width='2' className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'>
           <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
