e8e9a535c37af7d46f8e55f88f1f170f63660f4b
Oliver Caine <olliecaine@gmail.com>
2023-05-06 19:54:05 +1000

    More dev

--- src/ui/demos/RecipeSearch/RecipeSearchPage.tsx
+++ src/ui/demos/RecipeSearch/RecipeSearchPage.tsx
@@ -1,3 +1,7 @@
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ManageSearchIcon from '@mui/icons-material/ManageSearch'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import { IRecipe } from '@olivercaine/recipe-data/types'
 import React from 'react'
 import { AppHeader, FooterNav, Pagination, Recipes } from '../../../index'
@@ -20,5 +24,24 @@ export const RecipeSearchPage = ({
     <AppHeader appName={appName}/>
     <Recipes onImageClick={console.log} recipes={recipes}/>
     <Pagination/>
-    <FooterNav/>
+    <FooterNav>
+      <BottomNavigationAction
+        data-testid={'search'}
+        label='Search'
+        value='recents'
+        sx={{ color: '#a3a3a3', font: '#a3a3a3' }}
+        icon={<ManageSearchIcon />} />,
+      <BottomNavigationAction
+        data-testid={'favourites'}
+        label='Favorites'
+        value='favorites'
+        sx={{ color: '#a3a3a3', font: '#a3a3a3' }}
+        icon={<FavoriteIcon sx={{ color: '#a3a3a3', font: '#a3a3a3' }} />} />,
+      <BottomNavigationAction
+        data-testid={'shopping-list'}
+        label='Shopping List'
+        value='nearby'
+        sx={{ color: '#a3a3a3', font: '#a3a3a3' }}
+        icon={<ShoppingCartIcon />} />
+    </FooterNav>
   </>
