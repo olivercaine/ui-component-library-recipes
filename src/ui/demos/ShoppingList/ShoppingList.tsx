ce8c0d15a82593ce2fef36924e6cda92c4fe7be0
Oliver Caine <olliecaine@gmail.com>
2023-05-06 20:09:37 +1000

    Demos (#42)
    * Deleting unused images

--- src/ui/demos/ShoppingList/ShoppingList.tsx
+++ src/ui/demos/ShoppingList/ShoppingList.tsx
@@ -1,3 +1,7 @@
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ManageSearchIcon from '@mui/icons-material/ManageSearch'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import { IRecipe } from '@olivercaine/recipe-data/types'
 import React from 'react'
 import { ShoppingList } from '../../../index'
@@ -20,5 +24,24 @@ export const ShoppingListDemo: React.FC<{
   <>
     <AppHeader appName={appName}/>
     <ShoppingList onCheckboxChange={console.log} recipes={recipes} />
-    <FooterNav />
+    <FooterNav>
+      <BottomNavigationAction
+        data-testid={'search'}
+        label='Search'
+        value='recents'
+        sx={{ color: '#a3a3a3', font: '#a3a3a3' }}
+        icon={<ManageSearchIcon />} />,
+      <BottomNavigationAction
+        data-testid={'favourites'}
+        label='Favorites'
+        value='favorites'
+        sx={{ color: '#a3a3a3', font: '#a3a3a3' }}
+        icon={<FavoriteIcon sx={{ color: '#a3a3a3', font: '#a3a3a3' }} />} />,
+      <BottomNavigationAction
+        data-testid={'shopping-list'}
+        label='Shopping List'
+        value='nearby'
+        sx={{ color: '#a3a3a3', font: '#a3a3a3' }}
+        icon={<ShoppingCartIcon />} />
+    </FooterNav>
   </>
