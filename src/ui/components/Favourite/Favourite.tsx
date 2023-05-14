5d84996371b7aa716ac4ac943f762d016dffbf92
Oliver Caine <olliecaine@gmail.com>
2023-05-14 23:44:22 +1000

    Lint fix

--- src/ui/components/Favourite/Favourite.tsx
+++ src/ui/components/Favourite/Favourite.tsx
@@ -25,27 +25,27 @@ export const Favourite: FC<{
   onChangeCallback,
   value,
 }) => {
-    const [checked, setChecked] = useState<boolean>(checkedInit)
+  const [checked, setChecked] = useState<boolean>(checkedInit)
 
-    const dti = testIds({
-      uniqueId: value.toString()
+  const dti = testIds({
+    uniqueId: value.toString()
+  })
+
+  const onClickHandler = (/* e: UIEvent */) => {
+    setChecked(!checked)
+    onChangeCallback({
+      checked: !checked,
+      value
     })
-
-    const onClickHandler = (/* e: UIEvent */) => {
-      setChecked(!checked)
-      onChangeCallback({
-        checked: !checked,
-        value
-      })
-    }
-
-    return (
-      <div data-testid={dti.wrapper}>
-        <button data-testid={dti.button} onClick={onClickHandler} className={`rounded-full w-10 h-10 p-0 border-0 inline-flex items-center justify-center text-gray-500 ${css} ${checked ? 'bg-red-200' : 'bg-gray-200'}`}>
-          <svg fill={checked ? 'red' : 'gray'} strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' className='w-5 h-5' viewBox='0 0 24 24'>
-            <path d='M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z'></path>
-          </svg>
-        </button>
-      </div>
-    )
   }
+
+  return (
+    <div data-testid={dti.wrapper}>
+      <button data-testid={dti.button} onClick={onClickHandler} className={`rounded-full w-10 h-10 p-0 border-0 inline-flex items-center justify-center text-gray-500 ${css} ${checked ? 'bg-red-200' : 'bg-gray-200'}`}>
+        <svg fill={checked ? 'red' : 'gray'} strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' className='w-5 h-5' viewBox='0 0 24 24'>
+          <path d='M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z'></path>
+        </svg>
+      </button>
+    </div>
+  )
+}
