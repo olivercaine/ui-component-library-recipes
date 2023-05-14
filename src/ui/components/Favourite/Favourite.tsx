0b33027b1c7e96d19df03b12c7592906d1adfb7b
Oliver Caine <olliecaine@gmail.com>
2023-05-14 12:36:51 +1000

    Tidying up favourite

--- src/ui/components/Favourite/Favourite.tsx
+++ src/ui/components/Favourite/Favourite.tsx
@@ -35,7 +35,7 @@ export const Favourite: FC<{
     }
 
     return (
-      <button onClick={(e) => onClickHandler(e)} className={`rounded-full w-10 h-10 p-0 border-0 inline-flex items-center justify-center text-gray-500 ${css} ${checked ? 'bg-red-200' : 'bg-gray-200'}`}>
+      <button onClick={onClickHandler} className={`rounded-full w-10 h-10 p-0 border-0 inline-flex items-center justify-center text-gray-500 ${css} ${checked ? 'bg-red-200' : 'bg-gray-200'}`}>
         <svg fill={checked ? 'red' : 'gray'} stroke-linecap='round' stroke-linejoin='round' stroke-width='2' className='w-5 h-5' viewBox='0 0 24 24'>
           <path d='M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z'></path>
         </svg>
